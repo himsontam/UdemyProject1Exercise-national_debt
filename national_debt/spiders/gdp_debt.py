4b4cc9631b4907d8f97b32c6421ee288be7e69c2
Himson <himsom0528@gmail.com>
2020-10-24 22:16:44 +0800

    gdp_debt scrapy script

--- national_debt/spiders/gdp_debt.py
+++ national_debt/spiders/gdp_debt.py
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
-
 class GdpDebtSpider(scrapy.Spider):
     name = 'gdp_debt'
-    allowed_domains = ['worldpopulationreview.com/']
+    allowed_domains = ['worldpopulationreview.com']
     start_urls = ['http://worldpopulationreview.com/countries/countries-by-national-debt/']
 
     def parse(self, response):
-        pass
+        rows = response.xpath("//table/tbody/tr")
+        for row in rows:
+            countryName = row.xpath(".//td[1]/a/text()").get()
+            gdpRatio = row.xpath(".//td[2]/text()").get()
+            
+            yield {
+                'country_name' : countryName,
+                'gdp_debt' : gdpRatio
+            }
